7b380fd82bd2466fe35db075bac63d4c44b283d7
carolinna <112136079+carolinnaAna@users.noreply.github.com>
2022-11-30 19:48:35 -0300

    Add files via upload

---
+++ Caso do interruptor/switch caseMes.java
@@ -0,0 +1,34 @@
+importar  java . . _ Scanner ;
+public  class  Mes {
+
+	public  static  void  principal ( String [] args ) {
+		
+		Scanner  in = new  Scanner ( System . in );
+		
+		int  mes ;
+		Sistema . fora . println ( "Digite o numero do mes:" );
+		mes = em . proximoInt ();
+		
+		alternar   ( mes ) {
+			caso  1 :
+			caso  3 :
+			caso  5 :
+			caso  7 :
+			caso  8 :
+			caso  10 :
+			caso  12 :
+				Sistema . fora . println ( "O mes corresponde a: 31 dias." );
+				quebrar ;
+			caso  4 :
+			caso  6 :
+			caso  9 :
+			caso  11 :
+				Sistema . fora . println ( "O mes corresponde a: 30 dias." );
+				quebrar ;
+			caso  2 :
+				Sistema . fora . println ( "O mes corresponde a: 28 dias" );
+		em . fechar ();
+		}
+	}
+
+}
---
+++ Caso do interruptor/switch casePlaca.java
@@ -0,0 +1,38 @@
+importar  java . . _ Scanner ;
+public  class  Placa_veículo {
+	public  static  void  principal ( String [] args ) {
+		
+		Scanner  in = new  Scanner ( System . in );
+		
+			 placa int ;
+			Sistema . fora . println ( "Digite o ultimo numero da placa do carro" );
+			placa = em . proximoInt ();
+			
+			alternar ( placa ) {
+			caso  1 :
+			caso  2 :
+				Sistema . fora . println ( "Seu carro não pode circular na Segunda Feira." );
+				quebrar ;
+			caso  3 :
+			caso  4 :
+				Sistema . fora . println ( "Seu carro não pode circular na Terça-Feira." );
+			caso  5 :
+			caso  6 :
+				Sistema . fora . println ( "Seu carro não pode circular na Quarta-Feira." );
+				quebrar ;
+			caso  7 :
+			caso  8 :
+				Sistema . fora . println ( "Seu carro não pode circular na Quinta-Feira." );
+				quebrar ;
+			caso  9 :
+			caso  0 :
+				Sistema . fora . println ( "Seu carro não pode circular na Sexta-Feira." );
+				quebrar ;
+			padrão :
+				Sistema . fora . println ( "Número inválido" );
+		em . fechar ();
+
+			}
+	}
+
+}
